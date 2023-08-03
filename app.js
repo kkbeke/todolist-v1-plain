8d17f61068b4bde53046a203529790bcbbeb7ca2
Carlos Echavez <carlosechavezjr@gmail.com>
2023-08-03 22:57:04 +0800

    not finish

---
+++ app.js
@@ -0,0 +1,33 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+
+
+const week = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
+var today = new Date();
+var currentDay = today.getDay();
+var day = week[currentDay];
+
+// if(currentDay === 6 || currentDay === 0 ){
+//     day= "Weekend";
+// } else{
+//     day = "Weekday";
+// }
+
+    res.render('list' ,{kindOfDay: day});
+
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000..");
+});
